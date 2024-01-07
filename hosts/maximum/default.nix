2b7dad8c45a8ed0f0dac03e23087993e6b563423
Majes1ic <tom.awdry8@gmail.com>
2024-01-07 15:01:10 +0000

    add greetd

--- hosts/maximum/default.nix
+++ hosts/maximum/default.nix
@@ -8,13 +8,9 @@
     ../../modules/hardware/audio.nix
     ../../modules/hardware/networking.nix
     ../../modules/system/desktop.nix
+    ../../modules/system/greetd.nix
   ];
 
-  # home config
-  # home hyprland config
-  # go to arch and upload windows boot files for systemd-boot
-  # creating boot partition, mount to /boot
-  # generate and update hostID below using: 'head -c 8 /etc/machine-id'
   # add screenshot config
   
   networking.hostId = "150ce5b1";
