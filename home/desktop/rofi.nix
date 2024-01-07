b48b88009d0e359514c11d1c27f0cc750ffa787c
Majes1ic <tom.awdry8@gmail.com>
2024-01-07 15:02:27 +0000

    modify rofi launch keybind

--- home/desktop/rofi.nix
+++ home/desktop/rofi.nix
@@ -1,7 +1,7 @@
 {
   programs.rofi.enable = true;
   wayland.windowManager.hyprland.settings.bindr = [
-    #"SUPER, SUPER_L, exec, pkill rofi \|\| rofi"
-    "SUPER, SUPER_L, exec, rofi"
+    "SUPER, SUPER_L, exec, pkill rofi \|\| rofi -show drun"
+    #"SUPER, SUPER_L, exec, rofi"
   ];
 }
