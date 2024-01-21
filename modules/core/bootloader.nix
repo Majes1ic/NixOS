39e5e59b4e8ed9dfbe4b70a0f414993435ee3d9b
Majes1ic <tom.awdry8@gmail.com>
2024-01-21 17:43:25 +0000

    Adding windows boot entry

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -1,9 +1,26 @@
-{
-  boot = {
-    loader.systemd-boot = {
+{ pkgs, ... }: {
+  boot.loader = {
+    systemd-boot = {
       enable = true;
-      consoleMode = "keep";
+      consoleMode = "max";
+      extraFiles = {
+        #"efi/memtest86+/memtest86+.efi" = "${pkgs.memtest86plus}/memtest.efi";
+        "efi/edk2-shell/shellx64.efi" = pkgs.edk2-uefi-shell.efi;
+      };
+      extraEntries = {
+        #uncomment to add windows boot entry
+        #"edk2-shell.conf" = ''
+        #  title edk2-shell
+        #  efi /efi/edk2-shell/shellx64.efi
+        #'';
+          "windows.conf" = ''
+          title windows
+          efi /efi/windows/windows.nsh
+        '';
+      };
     };
-    loader.efi.canTouchEfiVariables = true;
+    efi = {
+      canTouchEfiVariables = true;
+    };
   };
 }
