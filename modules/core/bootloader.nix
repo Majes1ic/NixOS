6f41cb485f132ea8e2137c3781c4b2771615ad05
Majes1ic <tom.awdry8@gmail.com>
2024-03-10 16:12:44 +0000

    "Fixed" bootloader

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -5,17 +5,17 @@
       consoleMode = "max";
       extraFiles = {
         #"efi/memtest86+/memtest86+.efi" = "${pkgs.memtest86plus}/memtest.efi";
-        "efi/edk2-shell/shellx64.efi" = pkgs.edk2-uefi-shell.efi;
+        "EFI/edk2-shell/shellx64.efi" = pkgs.edk2-uefi-shell.efi;
       };
       extraEntries = {
         #uncomment to add windows boot entry
         #"edk2-shell.conf" = ''
         #  title edk2-shell
-        #  efi /efi/edk2-shell/shellx64.efi
+        #  efi /EFI/edk2-shell/shellx64.efi
         #'';
         "windows.conf" = ''
-          title windows
-          efi /efi/edk2-shell/shellx64.efi
+          title Windows
+          efi /EFI/edk2-shell/shellx64.efi
           options -nointerrupt -noconsolein -noconsoleout windows.nsh
         '';
       };
