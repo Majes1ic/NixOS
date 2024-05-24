37e5d438d2b5f44dd20daa2543d9099b334cf89c
Majes1ic <tom.awdry8@gmail.com>
2024-05-24 20:28:17 +0100

    Disabled rofi

--- home/desktop/rofi.nix
+++ home/desktop/rofi.nix
@@ -1,7 +1,8 @@
-{
-  programs.rofi.enable = true;
-  wayland.windowManager.hyprland.settings.bindr = [
-    "SUPER, SUPER_L, exec, pkill rofi \|\| rofi -show drun"
-    #"SUPER, SUPER_L, exec, rofi"
-  ];
-}
+#{
+#  programs.rofi.enable = true;
+#  #programs.rofi.theme = "Adapta-Nokto by PyGeek03";
+#  wayland.windowManager.hyprland.settings.bindr = [
+#    "SUPER, SUPER_L, exec, pkill rofi \|\| rofi -show drun"
+#    #"SUPER, SUPER_L, exec, rofi"
+#  ];
+#}
