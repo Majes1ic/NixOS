a3b9106e8cd951c1e98455347807ed9b5fb4b6af
Joshua Manchester <JManch@protonmail.com>
2024-05-27 16:15:40 +0100

    bootloader: refactor config to improve modularity
    Windows dual-boot config has been moved to a window.nix file. Filesystem
    config such as swap and trimming has been moved out of
    hardware-configuration.nix and into filesystem.nix.

--- hosts/maximum/default.nix
+++ hosts/maximum/default.nix
@@ -1,20 +1,17 @@
-# first module!!
-{ inputs, hostname, username, ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./hardware-configuration.nix
     ../../modules/core
+    ../../modules/hardware/windows.nix
     ../../modules/hardware/nvidia.nix
     ../../modules/hardware/audio.nix
     ../../modules/hardware/networking.nix
     ../../modules/system/gnome.nix
-    # ../../modules/system/greetd.nix
     ../../modules/programs/steam.nix
     ../../modules/programs/wireguard.nix
   ];
 
-  # add screenshot config
-  
   networking.hostId = "150ce5b1";
   system.stateVersion = "23.05";
 }
