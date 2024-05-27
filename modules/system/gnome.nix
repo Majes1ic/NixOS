53ee531a27bb5849b268c9e74b40775e5cc72f63
Joshua Manchester <JManch@protonmail.com>
2024-05-27 16:16:56 +0100

    gnome: disable power management and clean-up

--- modules/system/gnome.nix
+++ modules/system/gnome.nix
@@ -1,36 +1,37 @@
-{inputs, pkgs,, username, ... }: {
-  # imports = [
-  #   inputs.hyprland.nixosModules.default
-  # ];
-
-  # programs.hyprland.enable = true;
+{ pkgs, username, ... }: {
   # keyboard layout
   services.xserver.xkb.layout = "us";
 
-  services.xserver = {
-      enable = true;
-      displayManager.gdm.enable = true;
-      desktopManager.gnome.enable = true;
-  };
-
   programs.dconf.enable = true;
+
+  # Attempt to disable gnome power profiles
+  powerManagement.enable = false;
+  services.power-profiles-daemon.enable = false;
+
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  };
 
   environment.systemPackages = with pkgs; [
     gnomeExtensions.appindicator
     gnome.dconf-editor
   ];
 
-  # Change default color theme for all GTK4 applications to dark
-  home-manager.users.${username}.dconf = {
-    enable = true;
-    settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
-  };
+  home-manager.users.${username} = {
+    # Change default color theme for all GTK4 applications to dark
+    dconf = {
+      enable = true;
+      settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+    };
 
-  home-manager.users.${username}.gtk = {
-    enable = true;
-    iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
+    gtk = {
+      enable = true;
+      iconTheme = {
+        name = "Papirus-Dark";
+        package = pkgs.papirus-icon-theme;
+      };
     };
   };
 
