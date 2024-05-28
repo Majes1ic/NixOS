0ae5dce9fbac6b3a5b0737ed2c52440415ef65c0
Joshua Manchester <JManch@protonmail.com>
2024-05-28 14:43:54 +0100

    users: disable mutable users

--- modules/core/user.nix
+++ modules/core/user.nix
@@ -1,8 +1,8 @@
-{ username, pkgs, ... }:
+{ lib, pkgs, username, ... }:
 {
-  users.mutableUsers = true;
-  users.users = {
-    ${username} = {
+  users = {
+    mutableUsers = false;
+    users.${username} = {
       isNormalUser = true;
       shell = pkgs.zsh;
       hashedPassword = "$y$j7T$ZfkUr9Cl/SReyolJDGCgc.$NkCzfqNX/goO9cQ.2RnReh/O2WOm8EOIxhNQ4A/JIpD";
