536f27b7d8a2180c0047230b4b8b7faa7d218049
Joshua Manchester <JManch@protonmail.com>
2024-07-04 22:16:18 +0100

    steam: add proton-ge

--- modules/programs/steam.nix
+++ modules/programs/steam.nix
@@ -1,4 +1,15 @@
-{inputs, pkgs, ... }: {
-  programs.steam.enable = true;
+{ lib, pkgs, ... }:
+{
+  environment.systemPackages = [ pkgs.protontricks ];
+
   programs.gamemode.enable = true;
+
+  programs.steam = {
+    enable = true;
+    extraCompatPackages = [ pkgs.proton-ge-bin ];
+  };
+
+  # So that protontricks can find proton-ge
+  environment.sessionVariables.STEAM_EXTRA_COMPAT_TOOLS_PATHS =
+    lib.makeSearchPathOutput "steamcompattool" "" [ pkgs.proton-ge-bin ];
 }
