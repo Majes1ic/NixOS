d9cccfaf1df664e9f117bd2a26904ad876e4555d
Majes1ic <tom.awdry8@gmail.com>
2024-09-28 18:19:48 +0100

    Proton tricks fix

--- modules/programs/steam.nix
+++ modules/programs/steam.nix
@@ -11,5 +11,31 @@
 
   # So that protontricks can find proton-ge
   environment.sessionVariables.STEAM_EXTRA_COMPAT_TOOLS_PATHS =
-    lib.makeSearchPathOutput "steamcompattool" "" [ pkgs.proton-ge-bin ];
+    lib.makeSearchPathOutput "steamcompattool" ""
+      [ pkgs.proton-ge-bin ];
+  # Temporarily use appinfo_v29 branch to fix
+  # https://github.com/Matoking/protontricks/issues/304
+  nixpkgs.overlays = [
+    (final: prev: {
+      protontricks =
+        (prev.protontricks.overrideAttrs {
+          src = final.fetchFromGitHub {
+            owner = "Matoking";
+            repo = "protontricks";
+            rev = "f7b1fa33b0438dbd72f7222703f8442e40edc510";
+            hash = "sha256-t794WEMJx/JNX3gTMHfgquFWB7yXkleW07+QURm1NPM=";
+          };
+        }).override
+          {
+            vdf = final.python312Packages.vdf.overrideAttrs {
+              src = final.fetchFromGitHub {
+                owner = "Matoking";
+                repo = "vdf";
+                rev = "981cad270c2558aeb8eccaf42cfcf9fabbbed199";
+                hash = "sha256-OPonFrYrEFYtx0T2hvSYXl/idsm0iDPwqlnm1KbTPIo=";
+              };
+            };
+          };
+    })
+  ];
 }
